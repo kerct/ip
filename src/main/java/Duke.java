cdbd0e112b35be3b7a7310cd0c5901efaad3a7ea
kerct <kerchintian@gmail.com>
2020-08-26 11:16:36 +0800

    Rename variable 'line' to 'dottedLine'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,25 +7,25 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "\t-------------------------------------";
+        String dottedLine = "\t-------------------------------------";
         System.out.println(logo);
-        System.out.println(line);
+        System.out.println(dottedLine);
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
-        System.out.println(line);
+        System.out.println(dottedLine);
 
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
 
         while(!input.equalsIgnoreCase("bye")) {
-            System.out.println(line);
+            System.out.println(dottedLine);
             System.out.println("\t" + input);
-            System.out.println(line);
+            System.out.println(dottedLine);
             input = in.nextLine();
         }
 
-        System.out.println(line);
+        System.out.println(dottedLine);
         System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(dottedLine);
     }
 }
