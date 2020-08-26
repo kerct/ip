385bf3445dd839d5515272502edbc665f5a2596d
kerct <kerchintian@gmail.com>
2020-08-26 11:09:52 +0800

    Add echo and exit features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,13 +7,25 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "-------------------------------------";
+        String line = "\t-------------------------------------";
         System.out.println(logo);
         System.out.println(line);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        while(!input.equalsIgnoreCase("bye")) {
+            System.out.println(line);
+            System.out.println("\t" + input);
+            System.out.println(line);
+            input = in.nextLine();
+        }
+
+        System.out.println(line);
+        System.out.println("\tBye. Hope to see you again soon!");
         System.out.println(line);
     }
 }
