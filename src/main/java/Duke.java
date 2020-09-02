6bbdef9f6c7d76477937af48b12404b090d641c6
kerct <kerchintian@gmail.com>
2020-09-02 21:15:24 +0800

    Refactor code in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,86 +1,133 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String DOTTED_LINE = "\t-----------------------------------------";
+    private static final int MAX_TASKS = 100;
+    private static final String EXIT_COMMAND = "bye";
+    private static final String LIST_COMMAND = "list";
+    private static final String DONE_COMMAND = "done";
+    private static final String TODO_COMMAND = "todo";
+    private static final String DEADLINE_COMMAND = "deadline";
+    private static final String EVENT_COMMAND = "event";
+    private static final String BY_COMMAND = "/by";
+    private static final String AT_COMMAND = "/at";
+
+    private static final Task[] tasks = new Task[MAX_TASKS];
+    private static int numOfTasks = 0;
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String dottedLine = "\t-------------------------------------";
-        System.out.println(logo);
-        System.out.println(dottedLine);
-        System.out.println("\tHello! I'm Duke");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println(dottedLine);
+        printWelcomeMessage();
 
         Scanner in = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int numOfTasks = 0;
         String input = in.nextLine();
 
-        while (!input.equalsIgnoreCase("bye")) {
-            System.out.println(dottedLine);
-            if (input.equalsIgnoreCase("list")) {
-                printList(tasks, numOfTasks);
+        while (!input.equalsIgnoreCase(EXIT_COMMAND)) {
+            System.out.println(DOTTED_LINE);
+            if (input.equalsIgnoreCase(LIST_COMMAND)) {
+                handleInput(input, LIST_COMMAND);
             }
-            else if (input.startsWith("done")) {
-                int index = Integer.parseInt(input.substring(4).trim());
-                tasks[index - 1].markAsDone();
-                System.out.println("\tNice! I've marked this task as done: ");
-                System.out.println("\t" + tasks[index - 1].toString());
+            else if (input.startsWith(DONE_COMMAND)) {
+                handleInput(input, DONE_COMMAND);
             }
-            else if (input.startsWith("todo")) {
-                String name = input.substring(5).trim();
-                Task todo = new Todo(name);
-                tasks[numOfTasks] = todo;
-                numOfTasks++;
-                System.out.println("\tGot it. I've added this task:");
-                System.out.println("\t\t" + todo.toString());
-                System.out.println("\tNow you have " + numOfTasks + " tasks in the list.");
+            else if (input.startsWith(TODO_COMMAND)) {
+                handleInput(input, TODO_COMMAND);
             }
-            else if (input.startsWith("deadline")) {
-                int byIndex = input.indexOf("/by");
-                String name = input.substring(8, byIndex).trim();
-                String by = input.substring(byIndex + 3).trim();
-                Task deadline = new Deadline(name, by);
-                tasks[numOfTasks] = deadline;
-                numOfTasks++;
-                System.out.println("\tGot it. I've added this task:");
-                System.out.println("\t\t" + deadline.toString());
-                System.out.println("\tNow you have " + numOfTasks + " tasks in the list.");
+            else if (input.startsWith(DEADLINE_COMMAND)) {
+                handleInput(input, DEADLINE_COMMAND);
             }
-            else if (input.startsWith("event")) {
-                int atIndex = input.indexOf("/at");
-                String name = input.substring(5, atIndex).trim();
-                String at = input.substring(atIndex + 3).trim();
-                Task event = new Event(name, at);
-                tasks[numOfTasks] = event;
-                numOfTasks++;
-                System.out.println("\tGot it. I've added this task:");
-                System.out.println("\t\t" + event.toString());
-                System.out.println("\tNow you have " + numOfTasks + " tasks in the list.");
+            else if (input.startsWith(EVENT_COMMAND)) {
+                handleInput(input, EVENT_COMMAND);
             }
             else {
-                Task task = new Task(input);
-                tasks[numOfTasks] = task;
-                numOfTasks++;
-                System.out.println("\tadded: " + input);
+                handleInput(input, "");
             }
-            System.out.println(dottedLine);
+            System.out.println(DOTTED_LINE);
             input = in.nextLine();
         }
 
-        System.out.println(dottedLine);
-        System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println(dottedLine);
+        printEndMessage();
     }
 
-    private static void printList(Task[] tasks, int numOfTasks) {
+    private static void printWelcomeMessage() {
+        System.out.println(LOGO);
+        System.out.println(DOTTED_LINE);
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println(DOTTED_LINE);
+    }
+
+    private static void handleInput(String input, String command) {
+        switch(command) {
+        case LIST_COMMAND: {
+            printList();
+            break;
+        }
+        case DONE_COMMAND: {
+            int index = Integer.parseInt(input.substring(command.length()).trim());
+            Task task = tasks[index - 1];
+            markTaskAsDone(task);
+            break;
+        }
+        case TODO_COMMAND: {
+            String name = input.substring(TODO_COMMAND.length()).trim();
+            Task todo = new Todo(name);
+            addToTaskList(todo);
+            break;
+        }
+        case DEADLINE_COMMAND: {
+            int byIndex = input.indexOf(BY_COMMAND);
+            String name = input.substring(DEADLINE_COMMAND.length(), byIndex).trim();
+            String by = input.substring(byIndex + BY_COMMAND.length()).trim();
+            Task deadline = new Deadline(name, by);
+            addToTaskList(deadline);
+            break;
+        }
+        case EVENT_COMMAND: {
+            int atIndex = input.indexOf(AT_COMMAND);
+            String name = input.substring(EVENT_COMMAND.length(), atIndex).trim();
+            String at = input.substring(atIndex + AT_COMMAND.length()).trim();
+            Task event = new Event(name, at);
+            addToTaskList(event);
+            break;
+        }
+        default: {
+            Task task = new Task(input);
+            addToTaskList(task);
+            break;
+        }
+        }
+    }
+
+    private static void printList() {
         System.out.println("\tHere are the tasks in your list:");
         for (int i = 0; i < numOfTasks; i++) {
             Task task = tasks[i];
             System.out.println("\t" + (i + 1) + ". " + task.toString());
         }
+    }
+
+    private static void markTaskAsDone(Task task) {
+        task.markAsDone();
+        System.out.println("\tNice! I've marked this task as done: ");
+        System.out.println("\t" + task.toString());
+    }
+
+    private static void addToTaskList(Task task) {
+        tasks[numOfTasks] = task;
+        numOfTasks++;
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println("\t\t" + task.toString());
+        System.out.println("\tNow you have " + numOfTasks + " tasks in the list.");
+    }
+
+    private static void printEndMessage() {
+        System.out.println(DOTTED_LINE);
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println(DOTTED_LINE);
     }
 }
