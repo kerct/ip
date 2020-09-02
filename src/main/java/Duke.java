1b5b98b40db70bcb27f2f1827035a81c78c05959
kerct <kerchintian@gmail.com>
2020-09-02 13:45:24 +0800

    Add support for tracking three types of tasks: todo, deadline and event

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,7 +28,38 @@ public class Duke {
                 int index = Integer.parseInt(input.substring(4).trim());
                 tasks[index - 1].markAsDone();
                 System.out.println("\tNice! I've marked this task as done: ");
-                System.out.println("\t" + getTaskWithStatus(tasks[index - 1]));
+                System.out.println("\t" + tasks[index - 1].toString());
+            }
+            else if (input.startsWith("todo")) {
+                String name = input.substring(5).trim();
+                Task todo = new Todo(name);
+                tasks[numOfTasks] = todo;
+                numOfTasks++;
+                System.out.println("\tGot it. I've added this task:");
+                System.out.println("\t\t" + todo.toString());
+                System.out.println("\tNow you have " + numOfTasks + " tasks in the list.");
+            }
+            else if (input.startsWith("deadline")) {
+                int byIndex = input.indexOf("/by");
+                String name = input.substring(8, byIndex).trim();
+                String by = input.substring(byIndex + 3).trim();
+                Task deadline = new Deadline(name, by);
+                tasks[numOfTasks] = deadline;
+                numOfTasks++;
+                System.out.println("\tGot it. I've added this task:");
+                System.out.println("\t\t" + deadline.toString());
+                System.out.println("\tNow you have " + numOfTasks + " tasks in the list.");
+            }
+            else if (input.startsWith("event")) {
+                int atIndex = input.indexOf("/at");
+                String name = input.substring(5, atIndex).trim();
+                String at = input.substring(atIndex + 3).trim();
+                Task event = new Event(name, at);
+                tasks[numOfTasks] = event;
+                numOfTasks++;
+                System.out.println("\tGot it. I've added this task:");
+                System.out.println("\t\t" + event.toString());
+                System.out.println("\tNow you have " + numOfTasks + " tasks in the list.");
             }
             else {
                 Task task = new Task(input);
@@ -49,11 +80,7 @@ public class Duke {
         System.out.println("\tHere are the tasks in your list:");
         for (int i = 0; i < numOfTasks; i++) {
             Task task = tasks[i];
-            System.out.println("\t" + (i + 1) + ". " + getTaskWithStatus(task));
+            System.out.println("\t" + (i + 1) + ". " + task.toString());
         }
-    }
-
-    private static String getTaskWithStatus(Task task) {
-        return "[" + task.getStatusIcon() + "] " + task.getName();
     }
 }
