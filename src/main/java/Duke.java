48d06bff0575454f761efa2fa1060b365649e549
kerct <kerchintian@gmail.com>
2020-09-15 11:11:34 +0800

    Add feature to delete tasks from the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,15 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|\n";
     private static final String DOTTED_LINE =
             "\t----------------------------------------------------------";
-    private static final String EXIT_COMMAND = "bye";
-    private static final String LIST_COMMAND = "list";
-    private static final String DONE_COMMAND = "done";
     private static final String TODO_COMMAND = "todo";
     private static final String DEADLINE_COMMAND = "deadline";
     private static final String EVENT_COMMAND = "event";
     private static final String BY_COMMAND = "/by";
     private static final String AT_COMMAND = "/at";
+    private static final String LIST_COMMAND = "list";
+    private static final String DONE_COMMAND = "done";
+    private static final String DELETE_COMMAND = "delete";
+    private static final String EXIT_COMMAND = "bye";
 
     private static final ArrayList<Task> tasks = new ArrayList<>();
 
@@ -44,6 +45,8 @@ public class Duke {
                     executeCommand(input, LIST_COMMAND);
                 } else if (input.startsWith(DONE_COMMAND)) {
                     executeCommand(input, DONE_COMMAND);
+                } else if (input.startsWith(DELETE_COMMAND)) {
+                    executeCommand(input, DELETE_COMMAND);
                 } else if (input.startsWith(TODO_COMMAND)) {
                     executeCommand(input, TODO_COMMAND);
                 } else if (input.startsWith(DEADLINE_COMMAND)) {
@@ -68,9 +71,13 @@ public class Duke {
             break;
         }
         case DONE_COMMAND: {
-            int index = Integer.parseInt(input.substring(command.length()).trim());
-            Task task = tasks.get(index - 1);
-            markTaskAsDone(task);
+            int index = getIndexFromInput(input, command);
+            markTaskAsDone(index);
+            break;
+        }
+        case DELETE_COMMAND: {
+            int index = getIndexFromInput(input, command);
+            removeTask(index);
             break;
         }
         case TODO_COMMAND: {
@@ -115,10 +122,23 @@ public class Duke {
         }
     }
 
-    private static void markTaskAsDone(Task task) {
+    private static int getIndexFromInput(String input, String command) {
+        return Integer.parseInt(input.substring(command.length()).trim()) - 1;
+    }
+
+    private static void markTaskAsDone(int index) {
+        Task task = tasks.get(index);
         task.markAsDone();
         System.out.println("\tNice! I've marked this task as done: ");
         System.out.println("\t" + task.toString());
+    }
+
+    private static void removeTask(int index) {
+        Task task = tasks.get(index);
+        tasks.remove(index);
+        System.out.println("\tNoted. I've removed this task: ");
+        System.out.println("\t\t" + task.toString());
+        System.out.println("\tNow you have " + tasks.size() + " tasks in the list.");
     }
 
     private static void addToTaskList(Task task) {
